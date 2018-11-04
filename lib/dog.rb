afe1bd76ed0e97c03626d2369e21fae533f86c63
DANIELLE PARK <dpark1008@gmail.com>
2018-11-04 22:31:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
 class Dog
-  def initialize (breed)
+  def initialize (name, breed = "Mutt")
     @breed = breed
-  end
-    def breed=(breed)
-      @breed = breed
-    end
-      def breed
-        @breed
       end
     end
